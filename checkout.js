afe4a92307bffcf6e68a193595c1e2d0ba7e9744
daygonzz <54993737+daygonz@users.noreply.github.com>
2024-03-22 18:09:53 -0400

    Create checkout.js

---
+++ checkout.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('checkoutButton').addEventListener('click', function() {
+        // Checkout logic here
+        alert('Initiating Checkout...');
+    });
+});
